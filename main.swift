dc99a3d7428e684aa12d775b5beaed623d6bca6e
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-05-04 23:16:03 +0900

    Initialise

---
+++ main.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+
+
+func toTOC(read: (stripNewline: Bool) -> String?) -> String {
+  var toc: [(level: Int, word: String)] = []
+
+  while let line = read(stripNewline: true) {
+    if line.hasPrefix("```") {
+      while true {
+        guard let nextLine = read(stripNewline: true) else {
+          break
+        }
+        if nextLine == "```" {
+          break
+        }
+      }
+    } else {
+      if !line.hasPrefix("#") {
+        continue
+      }
+      var level = 0
+      for c in line.characters {
+        if c == "#" {
+          level += 1
+        } else {
+          break
+        }
+      }
+      let word = line[line.startIndex.advancedBy(level)..<line.endIndex]
+      toc.append((level: level, word: word))
+    }
+  }
+  return buildTOC(toc)
+}
+
+func buildTOC(toc: [(level: Int, word: String)]) -> String {
+  var res = ""
+  for (level, word) in toc {
+    res += String(count: (level-1) * 2, repeatedValue: Character(" "))
+    res += "- "
+    res += word
+    res += "\n"
+  }
+  return res
+}
+
+print(toTOC(readLine), terminator: "")
